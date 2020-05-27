7776737b5a697afe2af3694eb8878038861610c8
daniel <bitbucket@schimi-online.de>
2020-05-27 11:38:58 +0200

    fixed immoscout crawler

--- househunter/crawl_immobilienscout.py
+++ househunter/crawl_immobilienscout.py
@@ -78,8 +78,9 @@ class CrawlImmobilienscout:
                     'url': expose_urls[idx],
                     'title': title_el.text.strip().replace('NEU', ''),
                     'price': attr_els[0].text.strip().split(' ')[0].strip(),
-                    'size': attr_els[1].text.strip().split(' ')[0].strip() + " qm",
-                    'rooms': attr_els[2].text + " Zi.",
+                    'wsize': attr_els[1].text.strip().split(' ')[0].strip() + " qm",
+                    'rooms': attr_els[2].text.split(".")[-1],
+                    'hsize': attr_els[3].text.strip().split(' ')[0].strip() + " qm",
                     'address': address
                 }
             #print entries
